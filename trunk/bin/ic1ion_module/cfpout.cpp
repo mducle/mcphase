a4ffb5593a6c8760acc2948e0b1570c600ceb2a8
Duc Le <duc.le@helmholtz-berlin.de>
2009-09-15 09:11:14 +0000

    090915: ic1ionmodule - Added perl script to check output of cfp's for f-electrons with Cowan's tables.
    A    ic1ion_module/cfpout.cpp
    AM   ic1ion_module/testcfp.pl
    A    ic1ion_module/ING11.CFP
    
    
    git-svn-id: file:///home/mdl27/svn-rep/mcphase@246 f440dfc0-c60f-0410-a5d2-bf2cda3e4aac

---
+++ trunk/bin/ic1ion_module/cfpout.cpp
@@ -0,0 +1,66 @@
+/* cfpout.cpp
+ *
+ * Outputs the coefficient of fractional parentage calculated by ic1ion, for comparison with Cowan's value using the 
+ * perl script testcfp.pl
+ *
+ * Please use the testcfp.pl script, rather than compile and invoke this program separately.
+ *
+ * (c) 2009 Duc Le - duc.le@ucl.ac.uk
+ * This program is licensed under the GNU General Purpose License, version 2. Please see the COPYING file
+ */
+
+#include "ic1ion.hpp"
+#include <iomanip>
+
+double sign(double val) { return val<0?-1.:1.; }
+
+int main(int argc, char *argv[])
+{
+   int n,i,j,k,sz,szp,sc;
+   if(argc>1) { n = atoi(argv[1]); } else { n = 2; }
+   fconf conf(n,(orbital)3);
+   fconf confp(n-1,(orbital)3);
+   double cfp; int count, nlines;
+   std::vector<double> cfps; std::vector<int> jj;
+
+   sz = (int)conf.states.size(); szp = (int)confp.states.size();
+   for(j=0; j<szp; j++) std::cout << confp.states[j].id << " "; std::cout << "\n";
+   for(i=0; i<sz; i++)
+   {
+      cfps.clear(); jj.clear();
+      for(j=0; j<szp; j++)
+      {
+         cfp = racah_cfp(n,conf.states[i].U,conf.states[i].v,conf.states[i].S2,conf.states[i].L,confp.states[j].U,confp.states[j].v,confp.states[j].S2,confp.states[j].L);
+         if(fabs(cfp)>DBL_EPSILON) {
+            cfps.push_back(cfp); jj.push_back(j); }
+      }
+
+      nlines = ((int)cfps.size()-1)/4 + 1;
+      for(j=0; j<nlines; j++)
+      {
+         std::cout << conf.states[i].id; if(conf.states[i].id.size()==2) std::cout << "   "; else std::cout << "  ";  
+         if(j==(nlines-1)) std::cout << -(j+1) << "  "; else std::cout << " " << j+1 << "  ";
+         for(k=0; k<4; k++)
+         {
+            if((j*4+k+1)>cfps.size()) std::cout << "0   0.0000000000  ";
+            else 
+            {
+               std::cout.precision(10);
+               if((jj[j*4+k]+1)>9)
+               {
+                  if(cfps[j*4+k]<0) std::cout  << jj[j*4+k]+1 << " "  << std::fixed << cfps[j*4+k] << "  ";
+                  else              std::cout  << jj[j*4+k]+1 << "  " << std::fixed << cfps[j*4+k] << "  ";
+               }
+               else
+               {
+                  if(cfps[j*4+k]<0) std::cout  << jj[j*4+k]+1 << "  "  << std::fixed << cfps[j*4+k] << "  ";
+                  else              std::cout  << jj[j*4+k]+1 << "   " << std::fixed << cfps[j*4+k] << "  ";
+               }
+            }
+         }
+         std::cout << "F  " << n << "  " << i+1 << "\n";
+      }
+   }
+
+   return 0;
+}
---
+++ trunk/bin/ic1ion_module/testcfp.pl
@@ -0,0 +1,163 @@
+#!/usr/bin/perl
+
+unless (-e "ING11.CFP")   { die "Reference cfp tables in file ING11.CFP from RCG by Rober Cowan does not exist"; } 
+unless (-e "testcfp.exe") { system('g++ cfpout.cpp cfp.cpp states.cpp -o testcfp.exe')==0 or die "Fail to compile cfpout program."; }
+
+# Gets the block of cfps for the current number of f-electrons
+if ($#ARGV<0) { $n=2 } else { $n=$ARGV[0]; } 
+
+open(CFP,"ING11.CFP"); $startflag=0; $stopflag=0;
+while(<CFP>)
+{
+   chomp;
+   if($_=~/^\s*F/)
+   {
+      @Line = split;
+      if($n==$Line[1]) { $startflag=1; @headerline=@Line; }
+      if($n==$Line[1]-1) { $stopflag=1; }
+   }
+   if($startflag==1 && $stopflag==0)
+   {
+      push @cfprefblock, $_;
+   }
+   if(stopflag==1) { last; }
+}
+close(CFP);
+
+#print "Lines read: $#cfprefblock\n";
+
+$nchild = $headerline[2]; $nparent = $headerline[3]; @cfpref = (); @mycfpref = ();
+for $i (1..$nchild) { for $j (1..$nparent) { $cfpref[$i][$j] = 0; $mycfpref[$i][$j] = 0; } }
+$nparlines=int($nparent/20)+1; if($n<10) { $endcol=12; } else { $endcol=11; }
+
+$count=1; @childstates = ();
+for(@cfprefblock)
+{
+   @Line = split;
+   if($count>1 && $count<=($nparlines+1))
+   {
+      push @parentstates, @Line; 
+      if($n==8) { for $j (1..$#parentstates) 
+      { 
+         if($parentstates[$j] eq "2F0") { $parentstates[$j]="2F10"; } 
+         if($parentstates[$j] eq "2G0") { $parentstates[$j]="2G10"; last; } 
+      } }
+   }
+   elsif($count>$nparlines)
+   {
+      if(abs($Line[1])>999)
+      {
+         $n2=abs($Line[1])-abs(int($Line[1]/1000)*1000); $Line[1]=int($Line[1]/1000); splice(@Line,2,0,$n2);
+      }
+#     if($Line[4]>$nparent || ($Line[4]<1 && $Line[4]!=0) || $Line[6]>$nparent || ($Line[6]<1 && $Line[6]!=0) || $Line[8]>$nparent || ($Line[8]<1 && $Line[8]!=0)) 
+#     { print "@Line\n"; $sflag=1; } else { $sflag=0; }
+      if($Line[4]>$nparent || ($Line[4]<1 && $Line[4]!=0)) { $n2=int($Line[3]*1e10)/1e10; $n1=int(abs((abs($n2)-abs($Line[3]))*1e13)+0.5); splice(@Line,3,1,$n2,$n1); }
+      if($Line[6]>$nparent || ($Line[6]<1 && $Line[6]!=0)) { $n2=int($Line[5]*1e10)/1e10; $n1=int(abs((abs($n2)-abs($Line[5]))*1e13)+0.5); splice(@Line,5,1,$n2,$n1); }
+      if($Line[8]>$nparent || ($Line[8]<1 && $Line[8]!=0)) { $n2=int($Line[7]*1e10)/1e10; $n1=int(abs((abs($n2)-abs($Line[7]))*1e13)+0.5); splice(@Line,7,1,$n2,$n1); }
+#     if($sflag==1) { print " @Line\n"; }
+#     print join("\t",@Line),"\n";
+      if(abs($Line[1])==1) 
+      { 
+         if($Line[0] eq "2F0") { $Line[0]="2F10"; } 
+         if($Line[0] eq "2G0") { $Line[0]="2G10"; } 
+         $childstates[$Line[$endcol]-1] = $Line[0]; 
+      }
+      if($Line[2]!=0) { $cfpref[$Line[$endcol]-1][$Line[2]-1] = $Line[3]; }
+      if($Line[4]!=0) { $cfpref[$Line[$endcol]-1][$Line[4]-1] = $Line[5]; }
+      if($Line[6]!=0) { $cfpref[$Line[$endcol]-1][$Line[6]-1] = $Line[7]; }
+      if($Line[8]!=0) { $cfpref[$Line[$endcol]-1][$Line[8]-1] = $Line[9]; }
+   }
+   $count++;
+}
+
+# Determines the cfps calculated by ic1ion
+@mycfp = `./testcfp.exe $n`; $count=1; $ci=0;
+for(@mycfp)
+{
+   chomp; @Line = split;
+   if($count==1)
+   {
+      @myparentstates = @Line;
+      for $iL (0..$#parentstates) { for $jL (0..$#parentstates) 
+      {
+         if($Line[$jL] eq $parentstates[$iL]) { push @myparind, $jL; last; }
+      } }
+#     print join(":",@parentstates),"\n";
+#     print join(":",@myparentstates),"\n";
+#     for(@myparind) { print "$myparentstates[$_]:"; } print "\n";
+   }
+   else
+   {
+      if(abs($Line[1])==1) 
+      { 
+         for $iL (0..$#childstates) { if($Line[0] eq $childstates[$iL]) { $mychildstates[$iL] = $Line[0]; $ci=$iL; last; } }
+      }
+#     if($Line[2]!=0) { $mycfpref[$ci][$myparind[$Line[2]-1]] = $Line[3]; }
+#     if($Line[4]!=0) { $mycfpref[$ci][$myparind[$Line[4]-1]] = $Line[5]; }
+#     if($Line[6]!=0) { $mycfpref[$ci][$myparind[$Line[6]-1]] = $Line[7]; }
+#     if($Line[8]!=0) { $mycfpref[$ci][$myparind[$Line[8]-1]] = $Line[9]; }
+      if($Line[2]!=0) { $mycfpref[$ci][$Line[2]-1] = $Line[3]; }
+      if($Line[4]!=0) { $mycfpref[$ci][$Line[4]-1] = $Line[5]; }
+      if($Line[6]!=0) { $mycfpref[$ci][$Line[6]-1] = $Line[7]; }
+      if($Line[8]!=0) { $mycfpref[$ci][$Line[8]-1] = $Line[9]; }
+   }
+   $count++
+}
+
+#print join(":",@childstates),"\n";
+#print join(":",@mychildstates),"\n";
+#
+#print "    ["; for (0..$nparent-1) { print "$parentstates[$_]  \t\t"; } print "]\n";
+#for $i (0..$nchild-1)
+#{
+#   if(length($childstates[$i])==3) { print "$childstates[$i] " } else { print "$childstates[$i]  "; }
+#   print "[";
+#   for $j (0..$nparent-1)
+#   {
+#      if($cfpref[$i][$j]==0) { print "0  \t\t"; } else { print $cfpref[$i][$j],"\t"; }
+##     if($mycfpref[$i][$j]==0) { print "0\t\t"; } else { print $mycfpref[$i][$j],"\t"; }
+#   }
+#   print "]\n";
+#}
+#print "\n";
+#print "    ["; for (0..$nparent-1) { print "$myparentstates[$_]  \t\t"; } print "]\n";
+#for $i (0..$nchild-1)
+#{
+#   if(length($mychildstates[$i])==3) { print "$mychildstates[$i] " } else { print "$mychildstates[$i]  "; }
+#   print "[";
+#   for $j (0..$nparent-1)
+#   {
+##     if($cfpref[$i][$j]==0) { print "0\t\t"; } else { print $cfpref[$i][$j],"\t"; }
+##     if($mycfpref[$i][$j]==0) { print "0  \t\t"; } else { print $mycfpref[$i][$j],"\t"; }
+#      if($mycfpref[$i][$myparind[$j]]==0) { print "0  \t\t"; } else { print $mycfpref[$i][$myparind[$j]],"\t"; }
+#   }
+#   print "]\n";
+#}
+
+print "\t\t\tChecking cfp's for configuration f$n\n";
+print "--------------------------------------------------------------------------------------------------------\n";
+$sum=0; print "Child-Parent\tRCG_Index\tic1ion_Index\tRCG_Value\tic1ion_Value\tAbs(Difference)\n";
+print "--------------------------------------------------------------------------------------------------------\n";
+for $i (0..$nchild-1) { for $j (0..$nparent-1)
+{
+   $sum += abs($mycfpref[$i][$myparind[$j]]-$cfpref[$i][$j]);
+   if(abs($mycfpref[$i][$myparind[$j]]-$cfpref[$i][$j])>1e-6)
+   {
+      print "$childstates[$i]-$parentstates[$j]     \t(",$i+1,",",$j+1,")   \t(",$i+1,",",$myparind[$j]+1,")    \t";
+      if($cfpref[$i][$j]==0) { print "0\t\t"; } else { print "$cfpref[$i][$j]\t"; }
+      if($mycfpref[$i][$myparind[$j]]==0) { print "0\t\t"; } else { print "$mycfpref[$i][$myparind[$j]]\t"; }
+      print abs($mycfpref[$i][$myparind[$j]]-$cfpref[$i][$j]),"\n";
+   }
+} }
+print "--------------------------------------------------------------------------------------------------------\n";
+print "Sum of Abs(Difference) = $sum\n";
+print "--------------------------------------------------------------------------------------------------------\n";
+
+#$np=$n+1;
+#$linestart = `grep -n '  F   $n' ING11.CFP`;  chomp($linestart);
+#$linestop  = `grep -n '  F   $np' ING11.CFP`; chomp($linestop);
+#@tmp = split(/:/,$linestart); $start = $tmp[0];
+#@tmp = split(/:/,$linestop);  $stop  = $tmp[0];
+#open(CFP,"ING11.CFP"); @cfpref = <CFP>; close(CFP);
+#@cfpref = 
+#@mycfp = `./testcfp.exe $n`;
